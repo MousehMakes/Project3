a0e247964b8c14c491a8baffb9cd26fc05ee0bff
Jane Shelby Thompson <shelby.t@comcast.net>
2018-07-13 11:20:43 -0400

    new commit

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 var express = require('express');
 var app = express();
 
-var port = process.env.PORT || 8080;
-
-app.set('view engine', 'ejs');
+var port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname));
 
